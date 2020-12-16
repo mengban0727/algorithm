1695bd88aa677555dfd83ae684254616583805df
zhangjie <zhangjie@iyiou.com>
2020-12-16 20:37:52 +0800

    环形链表检测

---
+++ src/list/CycleList.java
@@ -0,0 +1,46 @@
+package list;
+
+
+/**
+ * 环形链表
+ *
+ * @author zhangjie
+ */
+public class CycleList {
+
+  class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+      val = x;
+    }
+  }
+
+  /**
+   * 检测环形链表
+   */
+  public boolean hasCycle(ListNode head) {
+    //只有一个节点
+    if (head == null || head.next == null) {
+      return false;
+    }
+
+    ListNode slow = head;
+    ListNode fast = head.next;
+    while (slow != fast) {
+      if (fast == null || fast.next == null) {
+        return false;
+      }
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    return true;
+
+
+  }
+
+
+}
