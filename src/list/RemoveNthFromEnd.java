9eff60112f526ae4242a8d5ff08e67e078f1d8f9
zhangjie <zhangjie@iyiou.com>
2020-12-16 21:06:25 +0800

    删除倒数第n个节点

---
+++ src/list/RemoveNthFromEnd.java
@@ -0,0 +1,44 @@
+package list;
+
+
+/**
+ * 删除倒数第n个节点，返回链表的头节点
+ *
+ * @author zhangjie
+ */
+public class RemoveNthFromEnd {
+
+  class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+      val = x;
+    }
+  }
+
+  /**
+   * 删除倒数第n个节点  1 2 3 4 5 6 7
+   */
+  public ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode slow = head;
+    ListNode fast = head;
+    for(int i = 0 ; i < n ; i++){
+      fast = fast.next;
+    }
+    //删除的是头节点
+    if(fast==null){
+     return head.next;
+    }
+
+    while (fast.next!=null){
+      slow = slow.next;
+      fast = fast.next;
+    }
+
+    //删除的slow+1 处的节点
+    slow.next = slow.next.next;
+    return head;
+  }
+}
