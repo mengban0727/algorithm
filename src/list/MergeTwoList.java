0020e0eae6e947a83948232379813fd927eb431c
zhangjie <zhangjie@iyiou.com>
2020-12-16 20:46:21 +0800

    合并两个有序链表

---
+++ src/list/MergeTwoList.java
@@ -0,0 +1,48 @@
+package list;
+
+
+/**
+ * 合并两个有序链表
+ *
+ * @author zhangjie
+ */
+public class MergeTwoList {
+
+  class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+      val = x;
+    }
+  }
+
+  /**
+   * 合并两个有序链表
+   */
+  public ListNode mergeTwoList(ListNode listNode1, ListNode listNode2) {
+    ListNode preNode = new ListNode(-1);
+    ListNode pre = preNode;
+    while (listNode1 != null && listNode2 != null) {
+      if (listNode1.val < listNode2.val) {
+        pre.next = listNode1;
+        listNode1 = listNode2.next;
+      } else {
+        pre.next = listNode2;
+        listNode2 = listNode2.next;
+      }
+      pre = pre.next;
+    }
+
+    if (listNode1 != null) {
+      pre.next = listNode1;
+    } else {
+      pre.next = listNode2;
+    }
+
+    return preNode.next;
+  }
+
+
+}
