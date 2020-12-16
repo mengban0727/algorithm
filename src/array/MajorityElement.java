55f60c97a24add9d17bdb49ee42edbbc8fe2119d
zhangjie <zhangjie@iyiou.com>
2020-12-16 22:55:53 +0800

    一个大小为 n 的数组，找到其中的多数元素（众数）

---
+++ src/array/MajorityElement.java
@@ -0,0 +1,28 @@
+package array;
+
+/**
+ * 众数-摩尔投票法
+ *
+ * @author zhangjie
+ */
+public class MajorityElement {
+
+  public int majorityElement(int[] nums) {
+    int majority = nums[0];
+    int count = 1;
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] == majority) {
+        count++;
+      } else {
+        count--;
+      }
+
+      if (count == 0) {
+        majority = nums[i];
+        count = 1;
+      }
+
+    }
+    return majority;
+  }
+}
