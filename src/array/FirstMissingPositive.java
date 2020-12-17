f4e1475ad96f3561dc1ac7b8a212b5d7d189eef6
zhangjie <zhangjie@iyiou.com>
2020-12-17 20:20:26 +0800

    一个未排序的整数数组，找出其中没有出现的最小的正整数

---
+++ src/array/FirstMissingPositive.java
@@ -0,0 +1,44 @@
+package array;
+
+/**
+ * 第一个缺失的正数
+ *
+ * @author zhangjie
+ */
+public class FirstMissingPositive {
+
+  public static int firstMissingPositive(int[] nums) {
+    int length = nums.length;
+
+    for (int i = 0; i < length; i++) {
+      //1,2,3,4
+      while (nums[i] >= 1 && nums[i] <= length && nums[i] != nums[nums[i] - 1]) {
+        //当前元素下标和当前元素应该在的元素下标交换
+        swap(nums, i, nums[i] - 1);
+      }
+    }
+
+    //遍历数组，找到最小正整数
+    for (int i = 0; i < length; i++) {
+      if (nums[i] != i + 1) {
+        return i + 1;
+      }
+    }
+
+    return length + 1;
+
+
+  }
+
+  private static void swap(int[] nums, int i, int i1) {
+    int tmp = nums[i1];
+    nums[i1] = nums[i];
+    nums[i] = tmp;
+  }
+
+  public static void main(String[] args) {
+    int i = firstMissingPositive(new int[]{0, 1, 2});
+    System.out.println(i);
+  }
+
+}
