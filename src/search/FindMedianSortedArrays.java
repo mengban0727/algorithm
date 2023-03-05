2cfcfcf7fc2e539772beacbe10dde4315384d1f7
梦伴 <1103820783@qq.com>
2023-03-05 18:34:41 +0800

    Update FindMedianSortedArrays.java

--- src/search/FindMedianSortedArrays.java
+++ src/search/FindMedianSortedArrays.java
@@ -2,6 +2,8 @@ package search;
 
 /**
  * 寻找两个正序数组的中位数 https://leetcode.cn/problems/median-of-two-sorted-arrays/
+ 
+ * https://leetcode.cn/problems/median-of-two-sorted-arrays/solutions/8999/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-w-2/?orderBy=most_votes
  *
  * @author zhangjie
  */
